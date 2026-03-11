f9d3193174cc3a38d32d74871d99b81980379d66
faazdy <leorengifor@gmail.com>
2026-03-11 15:44:33 -0500

    feat: update server to redirect to documentation and remove HelloWorld component
    - Added a redirect from the root path to /docs/index.html in server.js
    - Removed HelloWorld.vue component from the frontend
    - Added index.html documentation for the GymSaaS backend API

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,6 +10,10 @@ sql`SELECT NOW()`
   .then(res => console.log("DB conectada:", res))
   .catch(err => console.error("Error DB:", err));
 
-app.listen(PORT, () => {
+app.get("/", (req, res)=>{
+    res.redirect("/docs/index.html")
+})
+
+app.listen(PORT, (req, res) => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
